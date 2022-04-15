49449f272730c50b5821613ee2bd4558dfea887e
LeszczyTom <leszczytom@gmail.com>
2022-04-15 18:47:08 +0200

    Unit testing de l'interface IPokemonTrainerFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,4 +1,36 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokemonTrainerFactoryTest {
+
+    IPokemonTrainerFactory pokemonTrainerFactory;
+    IPokedex pokedex;
+    IPokedexFactory pokedexFactory;
+
+    public IPokemonTrainerFactoryTest() {
+        pokemonTrainerFactory = Mockito.mock(IPokemonTrainerFactory.class);
+        pokedex = Mockito.mock(IPokedex.class);
+        pokedexFactory = Mockito.mock(IPokedexFactory.class);
+    }
+
+    @Test
+    public void createTrainerTest() {
+        PokemonTrainer pokemonTrainer = new PokemonTrainer("Trainer", Team.VALOR, pokedex);
+
+        Mockito.doReturn(pokemonTrainer).when(pokemonTrainerFactory).createTrainer("Trainer", Team.VALOR, pokedexFactory);
+
+        // expected PokemenTrainer
+        Assert.assertEquals(pokemonTrainer.getClass(), pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory).getClass());
+        // expected name
+        Assert.assertEquals("Trainer", pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory).getName());
+        // expected team
+        Assert.assertEquals(Team.VALOR, pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory).getTeam());
+        // expected pokedex
+        Assert.assertEquals(pokedex.getClass(), pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory).getPokedex().getClass());
+        // expected same size pokedex
+        Assert.assertEquals(pokedex.getPokemons().size(), pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory).getPokedex().getPokemons().size());
+    }
 }
